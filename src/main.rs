6424c4e52d426dfe2c7bfd76f6536a501ff3776c
Stephen Kelly <stevekm@users.noreply.github.com>
2019-08-06 20:34:01 -0400

    add tests

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,14 @@ impl Reader {
     }
 }
 
+fn pattern_match(pattern: &str, contents: &str) -> bool {
+    if contents.contains(pattern){
+        true
+    } else {
+        false
+    }
+}
+
 fn main() {
     let matches = App::new("grep")
                         .about("GNU grep clone")
@@ -38,11 +46,40 @@ fn main() {
     for line in reader.get().lines() {
         match line {
             Ok(l) => {
-                if l.contains(pattern){
+                if pattern_match(&pattern, &l) {
                     println!("{}", l);
                 }
             }
             Err(e) => println!("error parsing line: {:?}", e),
         }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn demo() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_pattern_match1(){
+        let pattern = "foo";
+        let contents = "foobarbaz";
+        let output = pattern_match(&pattern, &contents);
+        let expected_output = true;
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn test_pattern_match2(){
+        let pattern = "foo";
+        let contents = "barbaz";
+        let output = pattern_match(&pattern, &contents);
+        let expected_output = false;
+        assert_eq!(output, expected_output);
     }
 }
